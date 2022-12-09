cff2c050aef7775d212052c2194260a14968a479
Bionus <bio.nus@hotmail.fr>
2022-12-10 00:25:25 +0100

    fix: use double quotes in casks

--- Casks/imgbrd-grabber-nightly.rb
+++ Casks/imgbrd-grabber-nightly.rb
@@ -1,15 +1,15 @@
-cask 'imgbrd-grabber-nightly' do
+cask "imgbrd-grabber-nightly" do
     version :latest
     sha256 :no_check
 
-    url 'https://github.com/Bionus/imgbrd-grabber/releases/download/nightly/Grabber_nightly.dmg'
-    name 'Grabber'
-    desc 'Very customizable imageboard/booru downloader with powerful filenaming features'
-    homepage 'https://bionus.github.io/imgbrd-grabber/'
+    url "https://github.com/Bionus/imgbrd-grabber/releases/download/nightly/Grabber_nightly.dmg"
+    name "Grabber"
+    desc "Very customizable imageboard/booru downloader with powerful filenaming features"
+    homepage "https://bionus.github.io/imgbrd-grabber/"
 
-    conflicts_with cask: 'imgbrd-grabber'
+    conflicts_with cask: "imgbrd-grabber"
 
-    app 'grabber.app'
+    app "grabber.app"
 
     zap trash: "~/Library/Preferences/Bionus/Grabber",
         rmdir: "~/Library/Preferences/Bionus"
--- Casks/imgbrd-grabber.rb
+++ Casks/imgbrd-grabber.rb
@@ -1,15 +1,15 @@
-cask 'imgbrd-grabber' do
-    version '7.9.1'
+cask "imgbrd-grabber" do
+    version "7.9.1"
     sha256 :no_check
 
-    url 'https://github.com/Bionus/imgbrd-grabber/releases/download/v#{version}/Grabber_v#{version}.dmg'
-    name 'Grabber'
-	desc 'Very customizable imageboard/booru downloader with powerful filenaming features'
-    homepage 'https://bionus.github.io/imgbrd-grabber/'
+    url "https://github.com/Bionus/imgbrd-grabber/releases/download/v#{version}/Grabber_v#{version}.dmg"
+    name "Grabber"
+	desc "Very customizable imageboard/booru downloader with powerful filenaming features"
+    homepage "https://bionus.github.io/imgbrd-grabber/"
 
-	conflicts_with cask: 'imgbrd-grabber-nightly'
+	conflicts_with cask: "imgbrd-grabber-nightly"
 
-    app 'grabber.app'
+    app "grabber.app"
 
 	zap trash: "~/Library/Preferences/Bionus/Grabber",
 		rmdir: "~/Library/Preferences/Bionus"
