a8d80c2301070de8ee63ec28cc8f4f61fe654e2b
Bionus <bio.nus@hotmail.fr>
2022-12-10 13:39:41 +0100

    chore: add hash to stable release

--- Casks/imgbrd-grabber.rb
+++ Casks/imgbrd-grabber.rb
@@ -1,6 +1,6 @@
 cask "imgbrd-grabber" do
     version "7.9.1"
-    sha256 :no_check
+    sha256 "1ed5dd3ba2f7dcadaf3d051229f7b14cc77466a05a17ed32e58a73b4f7c27c17"
 
     url "https://github.com/Bionus/imgbrd-grabber/releases/download/v#{version}/Grabber_v#{version}.dmg"
     name "Grabber"
