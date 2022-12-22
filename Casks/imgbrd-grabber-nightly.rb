65897637158eee8ce581265d97d3b3e42d5331ec
Bionus <bio.nus@hotmail.fr>
2022-12-22 01:48:51 +0100

    fix: add 'verified' flag to URL since homepage differ

--- Casks/imgbrd-grabber-nightly.rb
+++ Casks/imgbrd-grabber-nightly.rb
@@ -2,10 +2,11 @@ cask "imgbrd-grabber-nightly" do
     version :latest
     sha256 :no_check
 
-    url "https://github.com/Bionus/imgbrd-grabber/releases/download/nightly/Grabber_nightly.dmg"
+    url "https://github.com/Bionus/imgbrd-grabber/releases/download/nightly/Grabber_nightly.dmg",
+        verified: "github.com/Bionus/imgbrd-grabber/"
     name "Grabber"
     desc "Very customizable imageboard/booru downloader with powerful filenaming features"
-    homepage "https://bionus.github.io/imgbrd-grabber/"
+    homepage "https://www.bionus.org/imgbrd-grabber/"
 
     conflicts_with cask: "imgbrd-grabber"
 
